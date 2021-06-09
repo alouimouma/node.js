1bcf70bfcc4bd2799a72f4ea0441ac683fd4fa77
alouimouma <mohamedaloui401@gmail.com>
2021-06-09 11:33:54 -1200

    ajout le checkpoint

---
+++ filtered-ls.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+fs.readdir(process.argv[2], function (err, list) {
+    if (err) {
+        return console.error(err);
+    }
+    list.forEach((file) => {
+        path.extname(file) == "." + process.argv[3] ? console.log(file) : null;
+    });
+});
